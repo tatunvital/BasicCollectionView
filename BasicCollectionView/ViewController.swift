00f601de253037e55043ebc8c59c87cc3faef629
Vitali <vitalik221@yandex.ru>
2022-02-28 09:36:45 +0300

    Initial Commit

---
+++ BasicCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicCollectionView
+//
+//  Created by Виталий Татун on 28.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
